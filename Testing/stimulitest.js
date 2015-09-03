6fe1a8d1401d8f53742924fe1f6d65d7acf7ed3b
Leon Chin <leonchin499@gmail.com>
2015-09-03 21:54:55 +0800

    more stimuli

--- Testing/stimulitest.js
+++ Testing/stimulitest.js
@@ -2,7 +2,28 @@ var _ = require('lodash');
 var stimuli = require("../data/stimuli.json")
 
 var english = stimuli.english;
+var latin = stimuli.latin;
 
-var val = _.shuffle(english.paragraph);
 // console.log(val);
-console.log(english);
+// console.log(latin);
+// console.log("\n\n\n\n");
+
+// var latParaData = latin.paragraph;
+// var latSentData = latin.sentence;
+// var latPhraData = latin.phrase;
+
+// latParaData = _.shuffle(latParaData);
+// latSentData = _.shuffle(latSentData);
+// latPhraData = _.shuffle(latPhraData);
+
+// console.log(latParaData.length);
+// console.log(latSentData.length);
+// console.log(latPhraData.length);
+
+var engParaData = english.paragraph;
+var engSentData = english.sentence;
+var engPhraData = english.phrase;
+
+console.log(engParaData.length);
+console.log(engSentData.length);
+console.log(engPhraData.length);
